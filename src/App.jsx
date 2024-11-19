334280ba149d29cded8f609c4f323b33e679a9bf
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-11-19 23:38:43 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 
 import './App.css'
-import { Route,Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { Home } from './Pages/Home'
 import { Appointment } from './Pages/Appointment'
 import { About } from './Pages/About'
 import { Register } from './Pages/Register'
 import { Login } from './Pages/Login'
+import { Navbar } from './Components/Navbar'
 
 function App() {
 
   return (
     <>
-     <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/appointment" element={<Appointment/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/register" element={<Register/>} />
-      <Route path="/login" element={<Login/>} />
-     </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,30 @@
 import React, { useContext, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { AppContext } from '../Context/AppContext'
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 export const Navbar = () => {
   const [show, setShow] = useState(false)
-  const { isAuthenticated } = useContext(AppContext);
+  const { isAuthenticated,setIsAuthenticated } = useContext(AppContext);
+
+  const handleLogout=async()=>{
+    await axios.get('http://localhost:4000/api/v1/user/patient/logout',{
+      withCredentials:true,
+    }).then(res=>{
+      toast.success(res.data.message)
+      setIsAuthenticated(true)
+    })
+    .catch(err=>{
+      toast.error(err.response.data.message)
+    })
+  }
+
+  const navigate=useNavigate()
+
+  const handleLogin=()=>{
+    navigate('/login')
+  }
 
   return (
     <nav className='container'>
